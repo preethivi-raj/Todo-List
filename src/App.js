fa90ddf79a3fde52be2d711088b5f7203a39c019
PREETHIVIRAJ <preethiviraj9943@gmail.com>
2024-07-18 13:16:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import List from "./List";
+
 
 function App() {
+    
+const [text , setText] = useState()
+const [datas , setDatas] = useState([  ])
+
+const handleAdd = ()=>{
+      if(text){
+        setDatas([...datas , text]) 
+        setText("")
+      } 
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center">
+      <h1 className="text-6xl m-4 font-serif" >Todo List</h1>
+      <div >
+        <input
+         type="text" 
+         value={text}
+         className="input input-bordered input-warning w-full max-w-xs m-3"
+         placeholder="Enter task"
+         onChange={(e)=>setText(e.target.value)}
+         />
+
+        <button className="btn btn-outline btn-warning" onClick={handleAdd} >Add ++</button>
+      </div>
+
+      <List datas={datas} setDatas={setDatas}/>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/List.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const List = ({ datas, setDatas }) => {
+  function hadnleDelete(index) {
+    const updatedData = datas.filter((data, ind) => ind !== index);
+    setDatas(updatedData);
+  }
+
+  const render = datas.map((data, index) => {
+    return (
+      <tr key={index} className="text-xl">
+        <td>{index + 1}</td>
+        <td>{data}</td>
+        <td>
+          <button className="btn btn-error" onClick={() => hadnleDelete(index)}>
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <div className="flex justify-center">
+      <div className="overflow-x-auto">
+        <table className="table">
+          <tbody>{render}</tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default List;
